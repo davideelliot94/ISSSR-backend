847947a4f6aa51577c49fdac02203ab788ed84f3
redefik <fe.viglietta@gmail.com>
2019-06-07 11:24:14 +0200

    Rinominato Sprint 0

--- src/main/java/com/isssr/ticketing_system/dto/TargetDto.java
+++ src/main/java/com/isssr/ticketing_system/dto/TargetDto.java
@@ -1,17 +1,7 @@
 package com.isssr.ticketing_system.dto;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import com.isssr.ticketing_system.entity.BacklogItem;
-import com.isssr.ticketing_system.entity.ScrumTeam;
-import com.isssr.ticketing_system.enumeration.TargetState;
 import com.isssr.ticketing_system.enumeration.TargetType;
-import com.isssr.ticketing_system.response_entity.JsonViews;
 import lombok.Data;
-import lombok.NonNull;
-
-import javax.persistence.*;
-import java.util.Collection;
-import java.util.List;
 
 @Data
 public class TargetDto {
@@ -20,15 +10,5 @@ public class TargetDto {
     private String version;
     private String description;
     private TargetType targetType;
-//    private String scrumTeamName;
-      private Long scrumTeamId;
-
-//    public TargetDto(Long id, String name, String version, String description, TargetType targetType, Long scrumTeamId) {
-//        this.id = id;
-//        this.name = name;
-//        this.version = version;
-//        this.description = description;
-//        this.targetType = targetType;
-//        this.scrumTeamId = scrumTeamId;
-//    }
+    private Long scrumTeamId;
 }
